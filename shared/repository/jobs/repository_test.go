260c0602023b31fe326039b8ed30ae38b70904a0
Eli Autumn Sanders <easanders@pm.me>
2021-10-16 23:52:26 -0400

    update tests

--- shared/repository/jobs/repository_test.go
+++ shared/repository/jobs/repository_test.go
@@ -118,7 +118,7 @@ func Test_EmployerRepository_GetEmployerJobs_IncorrectData(t *testing.T) {
 
 	repository := jobs.NewJobRegistry().GetJobRepository()
 	var totalpostsbought int
-	jobs, totalpostsbought, err := repository.GetEmployerJobs("")
+	jobs, err := repository.GetEmployerJobs("")
 
 	assert.Nil(jobs)
 	assert.Equal(totalpostsbought, -1)
@@ -136,7 +136,7 @@ func Test_EmployerRepository_GetEmployerJobs_CorrectData(t *testing.T) {
 	testhelper.Helper_RandomJob(employer, t)
 	testhelper.Helper_RandomJob(employer, t)
 
-	jobs, totalpostsbought, err := repository.GetEmployerJobs(employer.PublicID)
+	jobs, err := repository.GetEmployerJobs(employer.PublicID)
 
 	employer.TotalPostsBought = totalpostsbought
 	assert.Equal(len(jobs), 3)
@@ -196,7 +196,7 @@ func Test_EmployerRepository_EditJob_MissingJobPublicID(t *testing.T) {
 
 	employer := testhelper.Helper_RandomEmployer(t)
 
-	job, err := repository.EditJob(employer.PublicID, "", "", "", "", "", "", "", "", 0, 0)
+	job, err := repository.EditJob(employer.PublicID, "", "", "", "", "", "", true)
 
 	assert.Nil(job)
 	assert.NotNil(err)
@@ -208,7 +208,7 @@ func Test_EmployerRepository_EditJob_MissingEmployerPublicID(t *testing.T) {
 
 	repository := jobs.NewJobRegistry().GetJobRepository()
 
-	job, err := repository.EditJob("", "", "", "", "", "", "", "", "", 0, 0)
+	job, err := repository.EditJob("", "", "", "", "", "", "", true)
 
 	assert.Nil(job)
 	assert.NotNil(err)
@@ -224,7 +224,7 @@ func Test_EmployerRepository_EditJob_Correct(t *testing.T) {
 
 	job := testhelper.Helper_RandomJob(employer, t)
 
-	result, err := repository.EditJob(employer.PublicID, job.PublicID, "A Job", "full-time", "full-stack", "this is a job", "2021-09-04", "2021-10-01", "hourly", 40, 50)
+	result, err := repository.EditJob(employer.PublicID, job.PublicID, "A Job", "full-time", "full-stack", "this is a job", "2021-09-04", true)
 
 	assert.NotNil(result)
 	assert.Equal(result.Title, "A Job")
